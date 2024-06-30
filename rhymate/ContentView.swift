37794a6e18243904ba866adce572a2d8575da58c
ropfoo <ropfoo@gmail.com>
2024-07-01 01:19:07 +0200

    Initial Commit

---
+++ rhymate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rhymate
+//
+//  Created by Robert Pasdziernik on 01.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rhymate/rhymateApp.swift
@@ -0,0 +1,17 @@
+//
+//  rhymateApp.swift
+//  rhymate
+//
+//  Created by Robert Pasdziernik on 01.07.24.
+//
+
+import SwiftUI
+
+@main
+struct rhymateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
