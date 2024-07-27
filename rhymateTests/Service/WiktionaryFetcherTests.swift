b41b92bfaa6cbe6cd12b4e396e956bac4e65f9a5
ropfoo <ropfoo@gmail.com>
2024-07-27 13:14:19 +0200

    feat: extend wiktionary fetcher test with local storage case

--- rhymateTests/Service/WiktionaryFetcherTests.swift
+++ rhymateTests/Service/WiktionaryFetcherTests.swift
@@ -2,17 +2,34 @@ import XCTest
 @testable import rhymate
 
 final class WiktionaryFetcherTests: XCTestCase {
-
+    var wiktionaryDefinitionsStorage: WiktionaryDefinitionsStorage!
+    
     override func setUpWithError() throws {
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        wiktionaryDefinitionsStorage = WiktionaryDefinitionsStorage()
     }
-
+    
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        UserDefaults.standard.removeObject(forKey: wiktionaryDefinitionsStorage.WIKTIONARY_DEFINITIONS_KEY)
     }
-
+    
     func testGetWiktionaryDefinitionsSuccess() async throws {
+        let fetcher = createMockWiktionaryFetcher()
+        let url = URL(string: WIKTIONARY_API_URL.absoluteString + "/definition/best")!
+        let json = wiktionaryMockBest
+        
+        BlockTestProtocolHandler.register(url: url) { (request: URLRequest) -> (response: HTTPURLResponse, data:Data?) in
+            let response = HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: BlockTestProtocolHandler.httpVersion, headerFields: nil)!
+            return (response, Data(json.utf8))
+        }
+        
+        let result = try await fetcher.getDefinitions(forWord: "best")
+        XCTAssertEqual(result, wiktionaryMockBestResult)
+    }
+    
+    func testGetWiktionaryDefinitionsLocalResultSuccess() async throws {
+        let localData = wiktionaryMockBestResult
+        try wiktionaryDefinitionsStorage.mutate(.add, data: localData, key: "best")
+        
         let fetcher = createMockWiktionaryFetcher()
         let url = URL(string: WIKTIONARY_API_URL.absoluteString + "/definition/best")!
         let json = wiktionaryMockBest
@@ -39,7 +56,7 @@ final class WiktionaryFetcherTests: XCTestCase {
         let result = try await fetcher.getDefinitions(forWord: "best")
         XCTAssertEqual(result, [])
     }
-
+    
     func createMockWiktionaryFetcher() -> WiktionaryFetcher {
         let config = URLSessionConfiguration.ephemeral
         config.protocolClasses = [
